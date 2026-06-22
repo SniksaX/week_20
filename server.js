3529b87668aa29c5700c07fb6db80781435e415a
SniksaX <papoosep1@gmail.com>
2026-06-22 13:27:15 +0200

    [ADD] exo 4]

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const { createServer } = require('node:http');
+const express = require('express');
 
-const hostname = '127.0.0.1';
+const PORT = 3000;
+const app = express();
 
-const port = 3000;
-
-const server = createServer((req, res) => {
+app.get("/", (req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
-});
+})
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(PORT, () => {
+    console.log(`app listening on http://localhost:${PORT}`)
+})
