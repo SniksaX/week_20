99d0575fdcc9331ead703223b8dd64f62a4dfa5f
SniksaX <papoosep1@gmail.com>
2026-06-23 15:38:31 +0200

    [FIX] EXO 14

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -1,8 +1,11 @@
-
-import {prisma} from "../../app.js";
+import { prisma } from "../../app.js";
 
 export const getAll = async (req, res) => {
-    
-    const result = await prisma.wood.findMany();
-    res.status(302).json({result})
-}
+    try {
+        const result = await prisma.wood.findMany();
+        res.status(200).json({ result })
+    } catch (e) {
+        res.status(500).json({ message: e.message || "Error fetching woods" });
+        console.error("Error during fetch:", e);
+    }
+};
--- app/routes/wood.js
+++ app/routes/wood.js
@@ -4,5 +4,6 @@ import * as woodController from '../controllers/wood.js';
 const router = express.Router();
 
 router.get('/', woodController.getAll);
+router.get('/:hardness', woodController.)
 
 export default router;
