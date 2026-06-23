240a260e7e7b70b8f58f24b7314b1225d740005d
SniksaX <papoosep1@gmail.com>
2026-06-23 13:02:26 +0200

    [ADD] Exo 12

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,7 +1,23 @@
+import {prisma} from "../../app.js"
+
+export const signup = async (req, res) => {
+    const {firstName, lastName, email, password} = req.body
+    console.log(firstName, lastName, email, password)
+
+    const newUser = await prisma.user.create({
+            data: {
+                firstName,
+                lastName,
+                email,
+                password
+            }
+    });
+
+    console.log(newUser)
+
+    res.status(201).json({message: "User Created: ", data: newUser});
+};
+
 export const login = (req, res) => {
     res.send('You are logged in')
 }
-
-export const signup = (req, res) => {
-    res.send('You are signup');
-};
