029c31b8eed0bc0927f36d5324e1508a4a36f63b
SniksaX <papoosep1@gmail.com>
2026-06-23 14:04:43 +0200

    [FIX] exo 12, try catch

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,18 +1,23 @@
 import {prisma} from "../../app.js"
 
 export const signup = async (req, res) => {
-    const {firstName, lastName, email, password} = req.body
+    try {
+        const {firstName, lastName, email, password} = req.body
 
-    const newUser = await prisma.user.create({
-            data: {
-                firstName,
-                lastName,
-                email,
-                password
-            }
-    });
+        const newUser = await prisma.user.create({
+                data: {
+                    firstName,
+                    lastName,
+                    email,
+                    password
+                }
+        });
 
-    res.status(201).json({message: "User Created: ", data: newUser});
+        res.status(201).json({message: "User Created: ", data: newUser});
+    } catch {
+        res.status(500).json({message: "User not created"})
+        console.error("Error during creation")
+    }
 };
 
 export const login = (req, res) => {
