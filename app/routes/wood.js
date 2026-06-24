8229c10e165d3f3161136007f27ab6ab320f6b66
SniksaX <papoosep1@gmail.com>
2026-06-24 14:06:10 +0200

    [ADD] EXO 17.

--- app/routes/wood.js
+++ app/routes/wood.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import * as woodController from '../controllers/wood.js';
+import auth from '../middlewares/auth.js';
 
 const router = express.Router();
 
-router.get('/', woodController.getAll);
-router.get("/:hardness", woodController.getByHardness);
+router.get('/', auth, woodController.getAll);
+router.get("/:hardness", auth, woodController.getByHardness);
 
 export default router;
